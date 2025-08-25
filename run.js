a0e84f230bc5b7afb2a5e5521bd77abd65899ee7
x011-al <x011pagkor@gmail.com>
2025-08-25 07:04:01 -0700

    Update run.js

--- run.js
+++ run.js
@@ -4,8 +4,8 @@ const { NMiner } = require("./index.js");
 const POOL = "stratum+tcp://141.94.223.113:4052"; // alamat pool
 const WALLET = "49cg2BTsCdmBfUPsCrDsGmREn2diVYSKUahupm2bay5ZU3gmVTzuwgY7yhQcbYCdEeZXSHsYLZKLWXTR4DNR3xcJS29HszU";                  // alamat wallet
 const OPTIONS = {
-    threads: 1,   // jumlah thread CPU
-    mode: "LIGHT"  // atau "LIGHT"
+    threads: 72,   // jumlah thread CPU
+    mode: "FAST"  // atau "LIGHT"
 };
 
 // ✅ Panggil constructor dengan 3 argumen
